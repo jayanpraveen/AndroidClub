54e29c9fb0e1750ddfadff3619ec748d16766d26
jayanpraveen <jayanpraveen30@gmail.com>
2021-01-23 10:03:44 +0530

    added new packages

--- src/App.js
+++ src/App.js
@@ -1,27 +1,28 @@
-import React ,{Component} from 'react';
-import './App.css';
-import Navbar from './components/NavBar/NavBar';
-import { BrowserRouter as Router} from "react-router-dom";
-import SideBar from './components/SlideBar/sideBar';
-import VideoBanner from './components/Banner/Banner'
+import React, { Component } from "react";
+import "./App.css";
+import Navbar from "./components/NavBar/NavBar";
+import { BrowserRouter as Router } from "react-router-dom";
+import SideBar from "./components/SlideBar/sideBar";
+import VideoBanner from "./components/Banner/Banner";
+import Cards from "./components/Cards/CardComponent";
 
-class App extends Component 
-{ 
-  state ={ isOpen :false}
+class App extends Component {
+  state = { isOpen: false };
 
-  toggle =()=>{
-    this.setState({isOpen :!(this.state.isOpen)});
-  }
-  render(){
-  return (
-    <Router>
-    <div className="App">
-      <SideBar isOpen={this.state.isOpen} toggle ={this.toggle}/>
-      <Navbar toggle ={this.toggle}/>
-      <VideoBanner/>
-    </div>
-    </Router>
-  );
+  toggle = () => {
+    this.setState({ isOpen: !this.state.isOpen });
+  };
+  render() {
+    return (
+      <Router>
+        <div className="App">
+          <SideBar isOpen={this.state.isOpen} toggle={this.toggle} />
+          <Navbar toggle={this.toggle} />
+          <VideoBanner />
+          <Cards />
+        </div>
+      </Router>
+    );
   }
 }
 
---
+++ src/components/Cards/CardComponent.js
@@ -0,0 +1,97 @@
+import React from "react";
+import { Card, Button } from "react-bootstrap";
+import Carousel from "react-elastic-carousel";
+import style from "./StyleButtons.css";
+
+// * Add <MainCard... /> for adding a new card,
+// * the rendering and postion will be automatically added,
+// * Since its a single component no states are used.
+
+function CardComponent() {
+  return (
+    <Carousel breakPoints={breakPoints} style={style}>
+      <MainCard
+        header="Peace"
+        cardTitle="title"
+        cardText="Android is a mobile operating system based on a modified version of the Linux kernel and other open source software."
+        cardTitle="Cards Title"
+        cardFooter="Footer"
+        image="https://androidcommunity.com/wp-content/uploads/2019/06/Android-Studio-64-bit.jpg"
+      />
+
+      <MainCard
+        header="Cards Header"
+        cardTitle="title"
+        cardText="Android is a mobile operating system based on a modified version of the Linux kernel and other open source software."
+        cardTitle="Cards Title"
+        cardFooter="Footer"
+        image="https://androidcommunity.com/wp-content/uploads/2019/06/Android-Studio-64-bit.jpg"
+      />
+
+      <MainCard
+        header="Cards Header"
+        cardTitle="title"
+        cardText="Android is a mobile operating system based on a modified version of the Linux kernel and other open source software."
+        cardTitle="Cards Title"
+        cardFooter="Footer"
+        image="https://androidcommunity.com/wp-content/uploads/2019/06/Android-Studio-64-bit.jpg"
+      />
+
+      <MainCard
+        header="Cards Header"
+        cardTitle="title"
+        cardText="Android is a mobile operating system based on a modified version of the Linux kernel and other open source software."
+        cardTitle="Cards Title"
+        cardFooter="Footer"
+        image="https://androidcommunity.com/wp-content/uploads/2019/06/Android-Studio-64-bit.jpg"
+      />
+      <MainCard
+        header="Cards Header"
+        cardTitle="title"
+        cardText="Android is a mobile operating system based on a modified version of the Linux kernel and other open source software."
+        cardTitle="Cards Title"
+        cardFooter="Footer"
+        image="https://androidcommunity.com/wp-content/uploads/2019/06/Android-Studio-64-bit.jpg"
+      />
+      <MainCard
+        header="Cards Header"
+        cardTitle="title"
+        cardText="Android is a mobile operating system based on a modified version of the Linux kernel and other open source software."
+        cardTitle="Cards Title"
+        cardFooter="Footer"
+        image="https://androidcommunity.com/wp-content/uploads/2019/06/Android-Studio-64-bit.jpg"
+      />
+    </Carousel>
+  );
+}
+
+// Card layout
+const MainCard = (props) => (
+  <Card style={mainStyle}>
+    <Card.Header style={cardStyle}>{props.header}</Card.Header>
+    <Card.Img src={props.image} />
+
+    <Card.Body style={bodyStyle}>
+      <Card.Title style={{ color: "#D8F3DC" }}>{props.cardTitle}</Card.Title>
+      <Card.Text>{props.cardText}</Card.Text>
+      <Button variant="success" href="https://www.android.com">
+        Success
+      </Button>
+    </Card.Body>
+
+    <Card.Footer style={cardStyle}>{props.cardFooter}</Card.Footer>
+  </Card>
+);
+
+// Styles
+const mainStyle = { width: "25vmax", textAlign: "center" };
+const cardStyle = { background: "#081C15", color: "#fff" };
+const bodyStyle = { background: "#333", color: "#00ff00" };
+
+const breakPoints = [
+  { width: 1, itemsToShow: 1 },
+  { width: 550, itemsToShow: 2 },
+  { width: 1200, itemsToShow: 3 },
+];
+
+export default CardComponent;
